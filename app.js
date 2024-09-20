4d7df6310e1fe3b74463145527d06331865f185d
Wesley Mak <wesleymak005@gmail.com>
2024-09-19 23:51:55 -0400

    Created indexRouter

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//routes
+const indexrouter = require("./routes/indexrouter")
+
+app.use("/", indexrouter);
+
 const PORT = 3000;
+app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
