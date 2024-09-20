9891f8bbcbe492d1dad812a0536e6b1e2cc5ae46
Wesley Mak <wesleymak005@gmail.com>
2024-09-20 17:16:03 -0400

    Add routes, parse form data

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //routes
-const indexrouter = require("./routes/indexrouter")
+const indexrouter = require("./routes/indexrouter");
+const newMsgRouter = require("./routes/newMessageRouter");
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexrouter);
+app.use("/new", newMsgRouter);
+
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Port ${PORT}!`));
